0adb6ece0441faa61bca13b0ff0784fca6d45997
Noga Kedrai <nogak@sela.co.il>
2025-03-03 12:10:34 +0200

    added compute vars.tf

--- environments/dev/compute/variables.tf
+++ environments/dev/compute/variables.tf
@@ -2,20 +2,6 @@ variable "environment" {
   description = "name of current env"
   type=string
 }
-variable "vpc_cidr" {
-  description = "the cide"
-  type = string
-}
-
-variable "public_subnets" {
-  description = "the public subnets"
-  type = list(string)
-}
-
-variable "private_subnets" {
-  description = "the private subnets"
-  type = list(string)
-}
 
 variable "availability_zones" {
   description = "Availability zones"
