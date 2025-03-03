53ac5381f78f1df084de46a4137b493de64c1e80
Noga Kedrai <nogak@sela.co.il>
2025-03-03 11:58:19 +0200

    final main with basic modules

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,126 +13,39 @@ provider "aws" {
   profile = "terraform-user"
 }
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
-  tags = {
-    Name = "main-vpc"
-  }
+module "network" {
+  source = "../../modules/network"
+
+  environment     = var.environment
+  vpc_cidr        = var.vpc_cidr
+  public_subnets  = var.public_subnets #["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = var.private_subnets
+  availability_zones  = var.availability_zones
 }
 
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+module "security_groups"{
+  source= "../../modules/security"
 
-  tags = {
-    Name = "main-public-subnet"
-  }
+  environment = var.environment
+  vpc_id = module.network.vpc_id
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+module "application_load_balancer"{
+source = "../../modules/lb"
 
-  tags = {
-    Name = "main-igw"
-  }
+  environment = var.environment
+  vpc_id = module.network.vpc_id
+  subnet_ids = module.network.public_subnet_ids
+  security_group_ids= [module.security_groups.alb_security_group_id]
+  instance_ids      = module.ec2.instance_ids 
 }
 
-# Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+module "ec2"{
+source = "../../modules/compute"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "main-public-rt"
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-# Security Group
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-security-group"
-  description = "Security group for EC2 instance"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    description = "HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ec2-security-group"
-  }
-}
-
-resource "aws_route53_zone" "primary" {
-  name = "nogas-server.com"
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = aws_route53_zone.primary.name
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.web_server.public_ip]
-}
-
-# EC2 Instance
-resource "aws_instance" "web_server" {
-  ami           = "ami-04b4f1a9cf54c11d0"  # Amazon ubuntu in us-east-1
-  instance_type = "t2.micro"
-
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
-  user_data_replace_on_change = true
-
-
-user_data = <<-EOF
-  #!/bin/bash
-  apt-get update
-  apt-get install -y apache2
-  echo "<h1>Hello from Terraform</h1>" > /var/www/html/index.html
-    echo "<h1>${var.title}</h1>" > /var/www/html/index.html
-  systemctl start apache2
-  systemctl enable apache2
-  EOF
-
-  tags = {
-    Name = "web-server"
-  }
-
+  environment = var.environment
+  availability_zones= var.availability_zones
+  public_subnet_ids=module.network.public_subnet_ids
+  vpc_security_group_ids= [module.security_groups.ec2_security_group_id]
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,4 +1,23 @@
-variable "title" {
-  description = "the title"
-  type        = string
+variable "environment" {
+  description = "name of current env"
+  type=string
+}
+variable "vpc_cidr" {
+  description = "the cide"
+  type = string
+}
+
+variable "public_subnets" {
+  description = "the public subnets"
+  type = list(string)
+}
+
+variable "private_subnets" {
+  description = "the private subnets"
+  type = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
 }
