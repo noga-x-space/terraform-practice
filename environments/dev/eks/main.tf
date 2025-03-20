24881a73c89048e855e2d8597a24453e228a70cf
noga <nogak@sela.co.il>
2025-03-20 16:44:21 +0200

    removed eks

--- environments/dev/eks/main.tf
+++ environments/dev/eks/main.tf
@@ -22,14 +22,14 @@ data "aws_iam_role" "cluster_admin" {
   name = "cluster-admin"
 }
 
-module "eks" {
-  source = "../../../modules/eks"
+# module "eks" {
+#   source = "../../../modules/eks"
 
-  user_name = var.user_name
-  private_ec2_sg_id   = [data.terraform_remote_state.security.outputs.private_ec2_security_group_id]
-  public_ec2_sg_id    = [data.terraform_remote_state.security.outputs.public_ec2_security_group_id]
-  role_name           = data.aws_iam_role.cluster_admin.name
-  vpc                 = data.terraform_remote_state.network.outputs.vpc_id
-  public_subnets_ids  = data.terraform_remote_state.network.outputs.public_subnet_ids
-  private_subnets_ids = data.terraform_remote_state.network.outputs.private_subnets_ids
-}
+#   user_name = var.user_name
+#   private_ec2_sg_id   = [data.terraform_remote_state.security.outputs.private_ec2_security_group_id]
+#   public_ec2_sg_id    = [data.terraform_remote_state.security.outputs.public_ec2_security_group_id]
+#   role_name           = data.aws_iam_role.cluster_admin.name
+#   vpc                 = data.terraform_remote_state.network.outputs.vpc_id
+#   public_subnets_ids  = data.terraform_remote_state.network.outputs.public_subnet_ids
+#   private_subnets_ids = data.terraform_remote_state.network.outputs.private_subnets_ids
+# }
